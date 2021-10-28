d75a0ef7489cff52655e9b912c9c4455f22d1de5
DobDaniel <david.dobondi@moisenicoaraonline.ro>
2021-10-28 20:58:05 +0300

    first bugs fixed

--- js/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const webp = require('gulp-webp');
- 
-gulp.task('default', () =>
-    gulp.src('../assets/images/new/performance/*.png')
-        .pipe(webp())
-        .pipe(gulp.dest('../assets/images/new/performance/'))
-);
