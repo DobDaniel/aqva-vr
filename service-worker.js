411b503044c826331c01555e23d6bac2c62147d2
DobDaniel <david.dobondi@moisenicoaraonline.ro>
2021-11-07 21:10:00 +0200

    bug fix

--- service-worker.js
+++ service-worker.js
@@ -20,7 +20,6 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
 	'index.html',
-	"denis",
 	'./', // Alias for index.html
 	'./css/style.css'
 ];
